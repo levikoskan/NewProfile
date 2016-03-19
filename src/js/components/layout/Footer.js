7781902fd65ebfca12662af639143196fa272e68
levi koskan <levikoskan@gmail.com>
2016-03-19 06:55:30 -0500

    i added a sliding nav bar and updated some pics

--- src/js/components/layout/Footer.js
+++ src/js/components/layout/Footer.js
@@ -3,15 +3,15 @@ import React from "react";
 
 export default class Footer extends React.Component {
   render() {
-
+    const path = './public/images/links/'
 
     return (
       <footer >
-        <div class="row">
-          <div class="col-lg-12">
-            <p>Provided by Levi Koskan</p>
-          </div>
-        </div>
+            <div className="bottomBar">
+              <a href="https://github.com/levikoskan"><img className="linkIcon" src={path +"github.png"}/></a>
+              <a href="https://www.linkedin.com/in/levikoskan"><img className="linkIcon" src={path +"linkedin.png"}/></a>
+              <a href="https://about.me/levikoskan"><img className="linkIcon" src={path +"aboutMe.png"}/></a>
+            </div>
       </footer>
     );
   }
--- src/js/pages/Homes.js
+++ src/js/pages/Homes.js
@@ -4,36 +4,61 @@ import { Link } from "react-router";
 
 
 
+import MenuItem from "../components/MenuItem"
 
 
 export default class Featured extends React.Component {
   constructor() {
     super();
-
+      this.state = {
+        show: false
+      }
     }
 
     componentWillMount(){
       document.body.setAttribute("class", "homeBackGround")
-
     }
 
+    show() {
+      this.setState({
+        show: !this.state.show
+      });
+      // document.addEventListener('click', this.noShow.bind(this));
+    }
+    // noShow(){
+    //   document.removeEventListener('click', this.noShow.bind(this));
+    //   // this.setState({ show: false});
+    //   console.log('noShow says the show is' + this.state.show)
+    // }
 
   render() {
     return (
       <div>
         <div>
+            <div className="menu">
+              <div className={(this.state.show ? "visible" : "")}>
+
+                <MenuItem >Contact Me</MenuItem>
+                <MenuItem >Resume</MenuItem>
+                <MenuItem >Technologies Used</MenuItem>
+                <div className="menu-item" onClick={this.show.bind(this)}>Exit</div>
+              </div>
+            </div>
+
+        </div>
+        <div>
           <img src='../../public/images/nameText.png'/>
-          <img className="navCloud" src='../../public/images/navCloud.png'/>
+          <img className="navCloud" onClick={this.show.bind(this)} src='../../public/images/navCloud.png'/>
         </div>
         <div className="homeContainer">
           <Link to="projects">
 
-              <img src='../../public/images/sandCastle.png' />
+              <img src='../../public/images/sandCastle_Sign.png' />
 
           </Link>
           <Link to="about" >
 
-              <img src='../../public/images/tiki_Levi.png' />
+              <img src='../../public/images/tiki_Levi_Sign.png' />
 
           </Link>
         </div>
