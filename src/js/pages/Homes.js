e2273d44a53c3696021d1de145864dab61f8ebd5
levi koskan <levikoskan@gmail.com>
2016-03-22 18:18:10 -0500

    got event listner to work

--- src/js/pages/Homes.js
+++ src/js/pages/Homes.js
@@ -7,46 +7,64 @@ import { Link } from "react-router";
 import MenuItem from "../components/MenuItem"
 
 
+
 export default class Featured extends React.Component {
   constructor() {
     super();
       this.state = {
-        show: false
+        show: false,
+        resume: false
       }
+      this.x = this.noShow.bind(this);
     }
-
     componentWillMount(){
       document.body.setAttribute("class", "homeBackGround")
     }
-
+    componentWillUnmount() {
+     document.body.removeEventListener('click', this.x);
+   }
     show() {
+      console.log("show")
       this.setState({
         show: !this.state.show
       });
-      // document.addEventListener('click', this.noShow.bind(this));
     }
-    // noShow(){
-    //   document.removeEventListener('click', this.noShow.bind(this));
-    //   // this.setState({ show: false});
-    //   console.log('noShow says the show is' + this.state.show)
-    // }
+    showResume(){
+      this.setState({
+      resume: !this.state.resume,
+      show: false
+      });
+      document.body.addEventListener('click', this.x);
+    }
+
+    noShow(){
+      console.log("noShow")
+      document.body.removeEventListener('click', this.x);
+      this.setState({ show: false, resume: false});
+    }
 
   render() {
     const path = './public/images/links/'
     return (
       <div>
         <div>
-            <div className="menu">
+            <div onClick={this.showResume.bind(this)} className="menu">
               <div className={(this.state.show ? "visible" : "")}>
 
                 <MenuItem >Contact Me</MenuItem>
-                <MenuItem >Resume</MenuItem>
+                <div className="menu-item" onClick={this.showResume.bind(this)}><MenuItem resumeFunc={this.showResume.bind(this)}>Resume</MenuItem></div>
                 <MenuItem >Technologies Used</MenuItem>
                 <div className="menu-item" id="bottomBar">
                   <a href="https://www.linkedin.com/in/levikoskan"><img className="linkIcon" src={path +"linkedin.png"}/></a>
                   <a href="https://github.com/levikoskan"><img className="linkIcon" src={path +"github.png"}/></a>
                   <a href="https://about.me/levikoskan"><img className="linkIcon" src={path +"aboutMe.png"}/></a>
                 </div>
+              </div>
+            </div>
+
+            <div className="menuResume">
+              <div className={(this.state.resume ? "visible" : "")}>
+                <MenuItem><img id="resumeImg" src={path +"resume.png"}/></MenuItem>
               </div>
             </div>
 
